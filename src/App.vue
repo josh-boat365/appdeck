1397628eabb9a0a8ce7c9ae2d538d7a3938d132c
josh-boat365 <nyarko.boateng20@outlook.com>
2025-01-10 11:03:26 +0000

    base template with login page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,85 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+import AppDeckLogo from '/public/staffride-logo.png'
+import AyeyiTechLogo from '/public/AT-long-logo-dark.png'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="flex w-full  min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto mt-10  w-[10rem]" :src="AppDeckLogo" alt="Your Company">
+        <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-blue-950">Sign in
+          to your account</h2>
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <div class="bg-white px-10 py-12 rounded-lg border shadow-md mt-5 md:mx-auto md:w-full md:max-w-md">
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <label for="email" class="block text-sm/6 font-medium text-blue-950">Email address</label>
+            <div class="mt-2">
+              <input type="email" name="email" id="email" autocomplete="email" required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-blue-950 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+            </div>
+          </div>
+
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm/6 font-medium text-gray-950">Password</label>
+            </div>
+            <div class="mt-2">
+              <input type="password" name="password" id="password" autocomplete="current-password" required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-blue-950 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <label for="" class="flex gap-3 text-blue-950 items-center py-1">
+              <input type="checkbox" name="" id="">
+              Remember me
+            </label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+              in</button>
+          </div>
+
+          <div class="flex gap-2 items-center justify-center">
+            <hr class="w-1/4 border-gray-300">
+            Or Continue with
+            <hr class="w-1/4 border-gray-300">
+          </div>
+          <div class="flex gap-2 items-center justify-center">
+            <button type="button"
+              class="flex w-2/3 items-center gap-2 justify-center py-3 rounded-md bg-white border border-gray-200 px-3 text-md font-semibold text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <img width="25" height="25" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo" />
+              Google
+            </button>
+            <button type="button"
+              class="flex w-2/3 items-center gap-2 justify-center py-3 rounded-md bg-white border border-gray-200 px-3 text-md font-semibold text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <img width="25" height="25" src="https://img.icons8.com/fluency/48/microsoft-outlook-2019.png"
+                alt="microsoft-outlook-2019" />
+              Outlook
+            </button>
+
+          </div>
+        </form>
+
+      </div>
+      <p class="mt-10 text-center text-sm/6 text-gray-500">
+        Not a member?
+        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+      </p>
+      <div class="flex justify-center gap-2 items-center mt-3">
+        <h6 class="text-sm font-bold">POWERED BY</h6>
+        <img class=" w-[10rem]" :src="AyeyiTechLogo" alt="Your Company">
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Bricolage Grotesque', '"Helvetica Neue"', 'sans-serif'], // Default sans font
+      },
+      colors: {
+        primary: '#0A2540', // Add the custom default font color
+      },
+      textColor: {
+        DEFAULT: '#0A2540', // Set the default text color
+      },
+
+    },
   },
   plugins: [],
 }
-
